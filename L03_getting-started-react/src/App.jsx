96f8ea1b9941e5ca70dbe324f4fb36e828afa69f
Luisa Jean Padillon <padillonluisa391@gmail.com>
2026-07-18 15:35:07 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <>
+      <h1><b>ABOUT ME</b></h1>
+      <p>Full Name: Luisa Jean T. Padillon</p>
+      <p>Course and Year: Bachelor of Science in Information Systems student, 3rd Year</p>
+      <h3>One Fun Fact About Myself</h3>
+      <ul>
+        <li>I love really love blue so much that almost of my things are blue. Its me and the color blue against the world.</li>
+      </ul>
+      <p>So far, I've learned so much in this course. I learned how to do a readme portfolio so that my GitHub profile will be engaging and informative. From the past activities, I can say that I've improved a little after 1 and a half month of learning. It makes me want to learn more about this course. I hope I will use all of the knowledge I gained in the future.</p>
+    </>
+
+  )
+}
+
+export default App
